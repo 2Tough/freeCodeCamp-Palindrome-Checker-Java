a7dbdab1eae227ec159def215b89b17f00035387
Polaski <101113655+2Tough@users.noreply.github.com>
2024-09-24 00:37:26 +0300

    Changed scanner input logic

--- src/main/java/org/freCodeCamp/Main.java
+++ src/main/java/org/freCodeCamp/Main.java
@@ -2,6 +2,8 @@ package org.freCodeCamp;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 public class Main {
@@ -11,38 +13,46 @@ public class Main {
         JFrame jframe = new JFrame("Palindrome Checker");
         Scanner scanner = new Scanner(System.in);
 
-
-        String input = scanner.nextLine();
-            Checker ofWords = new Checker(input);
-            
-
-        jframe.setBounds(100,100,140,40);
+        jframe.setBounds(100, 100, 140, 40);
 
         JButton btn = new JButton("Submit");
-        btn.setBounds(100,100,130,30);
+        btn.setBounds(100, 100, 130, 30);
 
         JLabel label = new JLabel();
         label.setText("Enter word:");
-        label.setBounds(10,10,100,100);
+        label.setBounds(10, 10, 100, 100);
 
         JLabel label1 = new JLabel();
-        label1.setBounds(10,10, 200,100);
+        label1.setBounds(10, 10, 200, 100);
 
 
         JTextField textField = new JTextField();
-        textField.setBounds(110,50,130,30);
+        textField.setBounds(110, 50, 130, 30);
 
         jframe.add(btn);
-        jframe.add(label1);
+
         jframe.add(label);
         jframe.add(textField);
 
 
-        jframe.setSize(300,300);
+        jframe.setSize(300, 300);
         jframe.setLayout(null);
         jframe.setVisible(true);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+        Scanner textScan = new Scanner(textField.getText());
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String val = textField.getText();
+                System.out.println("testing");
+                System.out.println(val);
+                //label1.setText(input);
+                //jframe.add(label1);
+            }
+        });
 
     }
 }
